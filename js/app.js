016d1544b3ad830dcd869276443373373a80fd32
nastyakaply <99553591+nastyakaply@users.noreply.github.com>
2022-03-03 16:54:49 +0300

    first lab

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const a = 10
+let b = 11 //изменяемая
+
+const user = {
+    firstName: "Иван",
+    lastName: "Иванов",
+    age: 30,
+
+
+    isOld() {
+        return this.age >30
+    }
+}
+
+// console.log(user.isOld())
+
+// function isOld() {
+//
+// }
+//
+// const isOld = function () {
+//
+// }
+
